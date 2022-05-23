a074e812776de9e1118ce7e63df2de7c5fc447fd
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-05-23 13:00:02 +0200

    UI refactor

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <ncurses.h>
 
+void main_menu(WINDOW*);
+void insert_product(WINDOW*, LL_NODE*);
+
 int main()
 {
     LL_NODE *head = NULL;
@@ -16,69 +19,15 @@ int main()
 
 
     /* ncurses terminal interface */
-    initscr();
+    WINDOW *w = initscr();
 
-    /* title */
-    attron(A_STANDOUT);
-    printw("CCASH - v0.0.1\n\n");
-    attroff(A_STANDOUT);
-    refresh();
-
-    /* Menu */
-    attron(A_BOLD);
-    printw("Menu \n");
-    attroff(A_BOLD);
-
-    printw("(1) Insert new check\n");
-    printw("(2) See list of checks\n");
-
-    refresh();
+    main_menu(w);
     int ch = getch();
 
     /* Insert new check */
     if (ch == '1')
     {
-        /* Title */
-        erase();
-        attron(A_STANDOUT);
-        printw("Insert new check\n");
-        attroff(A_STANDOUT);
-
-        /* Insert date */
-        printw("\n Date (YYYY-MM-GG): \n");
-        char date_buf[11];
-        scanw("%s", date_buf);
-
-        /* Insert shop */
-        printw("Name of the shop: \n");
-        char shop_buf[50];
-        scanw("%s", shop_buf);
-
-        /* Insert product name */
-        printw("Name of the product: \n");
-        char prod_buf[30];
-        scanw("%s", prod_buf);
-
-        /* Insert amount */
-        printw("Price paid: \n");
-        float price_buf;
-        scanw("%f", &price_buf);
-
-        /* Insert cathegory */
-        printw("Cathegory: \n");
-        char cat_buf[30];
-        scanw("%s", cat_buf);
-
-        /* Create linked list node from buffers and insert */
-        Entry e;
-        strcpy(e.date, date_buf);
-        strcpy(e.shop, shop_buf);
-        strcpy(e.product_name, prod_buf);
-        e.amount = price_buf;
-        strcpy(e.cathegory, cat_buf);
-
-        ins_ordered(e, head);
-        
+        insert_product(w, head);
     }
     /* See list of previously inserted checks */
     else if (ch == '2')
@@ -124,4 +73,67 @@ int main()
     tsv_serialize(head, "database3.tsv");
 
     return 0;
+}
+
+void main_menu(WINDOW *w)
+{
+    /* title */
+    wattron(w, A_STANDOUT);
+    wprintw(w, "CCASH - v0.0.1\n\n");
+    wattroff(w, A_STANDOUT);
+    wrefresh(w);
+
+    /* Menu */
+    wattron(w, A_BOLD);
+    wprintw(w, "Menu \n");
+    wattroff(w, A_BOLD);
+
+    wprintw(w, "(1) Insert new check\n");
+    wprintw(w, "(2) See list of checks\n");
+
+    wrefresh(w);
+}
+
+void insert_product(WINDOW *w, LL_NODE *head)
+{
+    /* Title */
+    werase(w);
+    wattron(w, A_STANDOUT);
+    wprintw(w, "Insert new check\n");
+    wattroff(w, A_STANDOUT);
+
+    /* Insert date */
+    wprintw(w, "\n Date (YYYY-MM-GG): \n");
+    char date_buf[11];
+    wscanw(w, "%s", date_buf);
+
+    /* Insert shop */
+    wprintw(w, "Name of the shop: \n");
+    char shop_buf[50];
+    wscanw(w, "%s", shop_buf);
+
+    /* Insert product name */
+    wprintw(w, "Name of the product: \n");
+    char prod_buf[30];
+    wscanw(w, "%s", prod_buf);
+
+    /* Insert amount */
+    wprintw(w, "Price paid: \n");
+    float price_buf;
+    wscanw(w, "%f", &price_buf);
+
+    /* Insert cathegory */
+    wprintw(w, "Cathegory: \n");
+    char cat_buf[30];
+    wscanw(w, "%s", cat_buf);
+
+    /* Create linked list node from buffers and insert */
+    Entry e;
+    strcpy(e.date, date_buf);
+    strcpy(e.shop, shop_buf);
+    strcpy(e.product_name, prod_buf);
+    e.amount = price_buf;
+    strcpy(e.cathegory, cat_buf);
+
+    ins_ordered(e, head);
 }
